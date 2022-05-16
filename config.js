1b8f34bcabfbc1d5d26949148ab21c251726fcb6
SL-Alpha-X <malinduprabanjanaperis@gmail.com>
2022-05-16 07:16:46 +0530

    modules

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
-
-if (fs.existsSync('config.env')) require('dotenv')
-    .config({
-    path: './config.env'
-});
+const {JsonDB} = require('node-json-db');
+const {Config} = require('node-json-db/dist/lib/JsonDBConfig')
+const db = new JsonDB(new Config('db', true, false, '/'))
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
@@ -13,88 +12,86 @@ function convertToBool(text, fault = 'true') {
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './alphaX.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
 
+if ( fs.existsSync('config.env') ){
+
+    db.push('/SESSION', process.env._ALPHA_SESSION)
+    db.push('/HEROKU/HEROKU', convertToBool(process.env.HEROKU))
+    db.push('/HEROKU/API_KEY', process.process.env.HEROKU_API_KEY)
+    db.push('/HEROKU/APP_NAME', process.env.HEROKU_APP_NAME)
+    db.push('/WORKTYPE', process.env.WORK_TYPE)
+    db.push('/LANG', process.env.LANGUAGE.toUpperCase())
+    db.push('/HANDLERS', process.env.HANDLERS)
+    db.push('/U_NAME', process.env.USER_NAME)
+    db.push('/SUDO', process.env.SUDO)
+    db.push('/DEBUG', convertToBool(process.env.DEBUG))
+}
+
 module.exports = {
-    VERSION: 'V2 Global Stable',
-    PROXY: process.env.PROXY === undefined ? 'false' : process.env.PROXY,
+    VERSION: 'V.1 Global Stable',
+    BRANCH: 'main',
     GROUP: 'https://chat.whatsapp.com/ItIRSBUMN9t2lQzCpfAKWt',
-    SESSION: process.env._ALPHA_SESSION === undefined ? '' : process.env._ALPHA_SESSION,
-    ANTILINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
-    AUTOBIO: process.env.AUTO_BIO === undefined ? 'false' : process.env.AUTO_BIO,
-    GANSTYLE: process.env.GAN_IMAGE === undefined ? '' : process.env.GAN_IMAGE,
-    LANG: process.env.LANGUAGE === undefined ? 'EN' : process.env.LANGUAGE.toUpperCase(),
-    ALIVEMSG: process.env.ALIVE_MESSAGE === undefined ? 'default' : process.env.ALIVE_MESSAGE,
-    KICKMEMSG: process.env.KICKME_MESSAGE === undefined ? 'default' : process.env.KICKME_MESSAGE,
-    BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
-    ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
-    MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
-    NOLOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
-    AI_LILY: process.env.AI_LILY === undefined ? 'false' : process.env.AI_LILY,
-    BLOCKMSG: process.env.BLOCK_MESSAGE === undefined ? 'default' : process.env.BLOCK_MESSAGE,
-    UNBLOCKMSG: process.env.UNBLOCK_MESSAGE === undefined ? 'default' : process.env.UNBLOCK_MESSAGE,
-    UNMUTEMSG: process.env.UNMUTE_MESSAGE === undefined ? 'default' : process.env.UNMUTE_MESSAGE,
-    WORKTYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
-    PROMOTEMSG: process.env.PROMOTE_MESSAGE === undefined ? 'default' : process.env.PROMOTE_MESSAGE,
-    DEMOTEMSG: process.env.DEMOTE_MESSAGE === undefined ? 'default' : process.env.DEMOTE_MESSAGE,
-    BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
-    AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
-    HANDLERS: process.env.HANDLERS === undefined ? '^[.!;/#]' : process.env.HANDLERS,
-    SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
-    CAPTION: process.env.CAPTION === undefined ? '༺🚀 Mᴀᴅᴇ Bʏ Ａｌｐｈａ-Ｘ 🔮࿐' : process.env.CAPTION,
-    BOTNAME: process.env.BOT_NAME === undefined ? '≪━─━─━─━─🛡️─━─━─━─━≫\n          *🛸 Ａｌｐｈａ-Ｘ 🛸*\n        ╚╩══• •✠•❀•✠ • •══╩╝' : process.env.BOT_NAME,
-    BIONAME: process.env.BIO_NAME === undefined ? '━──🔥──╮• Ａｌｐｈａ-Ｘ •╭──🔥──━' : process.env.BIO_NAME,
-    CL_KEY: process.env.COMMAND_LIST_KEY === undefined ? 'alphaWA' : process.env.COMMAND_LIST_KEY,
-    AM_KEY: process.env.ALIVE_MSG_KEY === undefined ? 'alive' : process.env.ALIVE_MSG_KEY,
-    D_SONG: process.env.DOWNLOAD_SONG === undefined ? '*🄳ᴏᴡɴʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ sᴏɴɢ...* 📥' : process.env.DOWNLOAD_SONG,
-    U_SONG: process.env.UPLOAD_SONG === undefined ? '*🅄ᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ sᴏɴɢ...* 📤' : process.env.UPLOAD_SONG,
-    D_VIDEO: process.env.DOWNLOAD_VIDEO === undefined ? '*🄳ᴏᴡɴʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ᴠɪᴅᴇᴏ...* 📥' : process.env.DOWNLOAD_VIDEO,
-    U_VIDEO: process.env.UPLOAD_VIDEO === undefined ? '*🅄ᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ᴠɪᴅᴇᴏ...* 📤' : process.env.UPLOAD_VIDEO,
-    U_NAME: process.env.USER_NAME === undefined ? 'Enter Name 🥲' : process.env.USER_NAME,
-    C_EMOJI: process.env.COMMAND_EMOJI === undefined ? '🪀' : process.env.COMMAND_EMOJI,
-    D_EMOJI: process.env.DESC_EMOJI === undefined ? '🎗️' : process.env.DESC_EMOJI,
-    W_EMOJI: process.env.WARN_EMOJI === undefined ? '⚠️️' : process.env.WARN_EMOJI,
-    A_PIC: process.env.ALIVE_PIC === undefined ? 'Sending pp' : process.env.ALIVE_PIC,
-    CLIST_MSG: process.env.COMMAND_LIST_MSG === undefined ? '*You need to set COMMAND_CLIST_MSG!* 🔧' : process.env.COMMAND_LIST_MSG,
-    CL_PIC: process.env.COMMAND_LIST_PIC === undefined ? 'https://telegra.ph/file/dc8e4edc7230cedbdec2c.jpg' : process.env.COMMAND_LIST_PIC,
-    D_SONG_PIC: process.env.DOWNLOAD_SONG_PIC === undefined ? 'https://thumbs.dreamstime.com/b/stereo-headphones-glitch-effect-music-electronic-devise-vector-icon-night-party-background-164226152.jpg' : process.env.DOWNLOAD_SONG_PIC,
-    D_VIDEO_PIC: process.env.DOWNLOAD_VIDEO_PIC === undefined ? 'https://moddingunited.xyz/wp-content/uploads/2021/06/PicsArt_06-18-08.57.23-1024x633.jpg' : process.env.DOWNLOAD_VIDEO_PIC,
-    WLCM_GIF: process.env.WELCOME_GIF === undefined ? 'https://telegra.ph/file/338cb7f0b2b01e463efcd.mp4' : process.env.WELCOME_GIF,
-    GBYE_GIF: process.env.GOODBYE_GIF === undefined ? 'https://telegra.ph/file/88f85fe6850015ae49067.mp4' : process.env.GOODBYE_GIF,
-    BLOCKLINK: process.env.BLOCK_LINK === undefined ? 'false' : process.env.BLOCK_LINK,
-    BLINK_A: process.env.BLOCK_LINK_A === undefined ? 'chat.whatsapp.com' : process.env.BLOCK_LINK_A,
-    BLINK_B: process.env.BLOCK_LINK_B === undefined ? 'pornhub.com' : process.env.BLOCK_LINK_B,
-    ALB_MSG: process.env.ANTILINK_BAN_MSG === undefined ? 'default' : process.env.ANTILINK_BAN_MSG,
-    BGM: process.env.BGM === undefined ? 'false' : process.env.BGM,
-    CMENU: process.env.EXTRA_MENU_TITLE === undefined ? '෴ ɢᴜɪᴅᴇ 👀' : process.env.EXTRA_MENU_TITLE,
-    CMENU_MSG: process.env.EXTRA_MENU_MESSAGE === undefined ? 'Read The Guide. ℹ️ \n\n```Alpha-X Bot - Alpha Userbot is Open Source software open to development. \nThe user is responsible for all consequences that may arise from incorrect or misuse. \nSince it is an open source project, anyone can copy the software, add and remove,\nand use it in a way that they customize. In addition, plug-in support enables users to \ninstall their own plugins to the original software and use them as they wish.\nUsing the bot out of purpose will explicitly ban you.\nUsage is entirely the user\'s responsibility, Alpha Userbot is an \ninfrastructure only. Just as the operating system is not responsible \nfor the work done with the programs that are installed later, Alpha-X \nis not responsible for the usage purpose and method of the users.\nMarketing Alpha-X for money, making it available or having any material value\nıt is strictly forbidden to offer it for sale with anything. All legal investigations that may arise\nthe user is responsible.```' : process.env.EXTRA_MENU_MESSAGE,
-    XAPI: process.env.XTEAM_API_KEY === undefined ? '' : process.env.XTEAM_API_KEY,
-    BKICK: process.env.ANTI_BADWORD === undefined ? 'false' : process.env.ANTI_BADWORD,
-    BKICK_MSG: process.env.ANTI_BADWORD_KICK_MSG === undefined ? 'default' : process.env.ANTI_BADWORD_KICK_MSG,
-    NEW_SESSION: process.env._NEW_SESSON === undefined ? 'false' : process.env._NEW_SESSION,
-    BRANCH: 'multi-device',
-    HEROKU: {
-        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
-        API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
-        APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
-    },
+    PROXY: db.getData('/PROXY'),
+    SESSION: db.getData('/SESSION'),
+    ANTILINK: db.getData('/ANTILINK'),
+    AUTOBIO:db.getData('/AUTOBIO'),
+    GANSTYLE: db.getData('/GANSTYLE'),
+    LANG: db.getData('/LANG'),
+    ALIVEMSG: db.getData('/ALIVEMSG'),
+    KICKMEMSG: db.getData('/KICKMEMSG'),
+    BLOCKCHAT: db.getData('/BLOCKCHAT'),
+    ADDMSG: db.getData('/ADDMSG'),
+    MUTEMSG: db.getData('/MUTEMSG'),
+    NOLOG: db.getData('/NOLOG'),
+    AI_LILY: db.getData('/AI_LILY'),
+    BLOCKMSG: db.getData('/BLOCKMSG'),
+    UNBLOCKMSG:db.getData('/UNBLOCKMSG'),
+    UNMUTEMSG: db.getData('/UNMUTEMSG'),
+    WORKTYPE: db.getData('/WORKTYPE'),
+    PROMOTEMSG: db.getData('/PROMOTEMSG'),
+    DEMOTEMSG: db.getData('/DEMOTEMSG'),
+    BANMSG: db.getData('/BANMSG'),
+    AFKMSG: db.getData('/AFKMSG'),
+    HANDLERS: db.getData('/HANDLERS'),
+    SEND_READ: db.getData('/SEND_READ'),
+    CAPTION: db.getData('/CAPTION'),
+    BOTNAME: db.getData('/BOTNAME'),
+    BIONAME: db.getData('/BIONAME'),
+    CL_KEY: db.getData('/CL_KEY'),
+    AM_KEY: db.getData('/AM_KEY'),
+    D_SONG: db.getData('/D_SONG'),
+    U_SONG: db.getData('/U_SONG'),
+    D_VIDEO: db.getData('/D_VIDEO'),
+    U_VIDEO: db.getData('/U_VIDEO'),
+    U_NAME: db.getData('/U_NAME'),
+    C_EMOJI:db.getData('/C_EMOJI'),
+    D_EMOJI: db.getData('/D_EMOJI'),
+    W_EMOJI: db.getData('/W_EMOJI'),
+    A_PIC: db.getData('/A_PIC'),
+    CLIST_MSG: db.getData('/CLIST_MSG'),
+    CL_PIC: db.getData('/CL_PIC'),
+    D_SONG_PIC:db.getData('/D_SONG_PIC'),
+    D_VIDEO_PIC:db.getData('/D_VIDEO_PIC'),
+    WLCM_GIF: db.getData('/WLCM_GIF'),
+    GBYE_GIF: db.getData('/GBYE_GIF'),
+    BLOCKLINK: db.getData('/BLOCKLINK'),
+    BLINK_A: db.getData('/BLINK_A'),
+    BLINK_B: db.getData('/BLINK_B'),
+    ALB_MSG: db.getData('/ALB_MSG'),
+    BGM: db.getData('/BGM'),
+    CMENU: db.getData('/CMENU'),
+    CMENU_MSG: db.getData('/CMENU_MSG'),
+    XAPI: db.getData('/XAPI'),
+    BKICK: db.getData('/BKICK'),
+    BKICK_MSG: db.getData('/BKICK_MSG'),
+    HEROKU: db.getData('HEROKU'),
     DATABASE_URL: DATABASE_URL,
-    DATABASE: DATABASE_URL === './alphaX.db' ? new Sequelize({
-        dialect: "sqlite",
-        storage: DATABASE_URL,
-        logging: DEBUG
-    }) : new Sequelize(DATABASE_URL, {
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        },
-        logging: DEBUG
-    }),
-    RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? '8mn8zU7BedfTjsZzN64URZNy' : process.env.REMOVE_BG_API_KEY,
-    NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
-    SUDO: process.env.SUDO === undefined ? false : process.env.SUDO,
-    DEBUG: DEBUG,
-    WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG",
-    BOTHELP: "120363023256845651",
-    COMMUNITY: "120363022626781816"
-};
+    DATABASE: DATABASE_URL === './alphaX.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
+    RBG_API_KEY: db.getData('/RBG_API_KEY'),
+    NO_ONLINE: db.getData('/NO_ONLINE'),
+    SUDO: db.getData('/SUDO'),
+    DEBUG: db.getData('/DEBUG'),
+    COFFEEHOUSE_API_KEY: db.getData('/COFFEEHOUSE_API_KEY'),
+    WITAI_API: "TEYMELA6DMC4XB5YM3SPTTQWUUIBKURG", 
+    BOTHELP: "🙂",
+    COMMUNITY: "🙂"
+    };
